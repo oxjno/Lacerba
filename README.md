636a8449c8bcaff6f1ea3ed8ffd07de1d3bdb8f0
superdioz <julianus@oxjno.com>
2017-03-28 16:01:14 +0200

    Semplificato il caricamento dei feed

--- README.md
+++ README.md
@@ -3,13 +3,12 @@ Lacerba.io's feed reader
 
 This is a demo application developed as didactic purpose for the course "Mobile App development" on Lacerba.io
 
-It uses the following third party:
+It uses the following third party libraries:
 http://jquery.com/
 http://getbootstrap.com/
-http://www.jqueryscript.net/social-media/Tiny-jQuery-Based-RSS-Reader-Custom-Template-RSS-js.html
 
 To generate the icons in all size we use:
 http://pgicons.abiro.com/
 
-To modify the standard Wordpress feed we use:
-https://it.wordpress.org/plugins/sb-rss-feed-plus/
+To get the standard Wordpress feed in json we use:
+https://it.wordpress.org/plugins/feed-json/
--- js/main.js
+++ js/main.js
@@ -1,38 +1,56 @@
 $( document ).ready(function() {
-
-	//plugin jquery.rss.js http://www.jqueryscript.net/social-media/Tiny-jQuery-Based-RSS-Reader-Custom-Template-RSS-js.html
+	
 	/*Caricamento iniziale degli ultimi video*/
-	$("#rss-feeds")
-	  .hide()
-	  .rss("https://videoblog.lacerba.io/feed/"/*+Math.random(10000)*/, {
-		limit: 5,
-		effect: 'slideFastSynced',
-		layoutTemplate: '{entries}',
-		entryTemplate: '<div class="entry"><a href="{url}feed/?withoutcomments=1&fsk=58ce669950cc7" class="entry">{teaserImage}<h3>{title}</h3></a></div>'
-	  }, function() {
+	$.ajax({
+	  url: "https://videoblog.lacerba.io/feed/json?callback=callback",
+	  method: "GET",
+	  dataType: "jsonp",
+	  data: '',
+	  success: function( data, status, jqxhr ){
+		  console.log( data );
+		$.each(data, function(index, element) {
+           $('#rss-feeds').append($('<div class="entry"><a href="'+element.permalink+'feed/json" class="entry"><img src="'+element.thumbnail.split(",")[2].split(' 768w')[0]+'"><h3>'+element.title+'</h3></a></div>'
+            ));
+        });
 		$("#loader").hide();
 		$("#rss-feeds").show();
-	  });
+	  },
+	  error: function( jqxhr, status, error ){
+		console.log( "Something went wrong!" );
+	  }
+	});
 	
 	/*Click sul menu*/
 	$(".dropdown-menu a").click(function(e) {
 		e.preventDefault();
 		$(window).scrollTop(0);
+		categoryUrl = $(this).attr('href');
+		maxEntry = 100;	
+		if(categoryUrl=="https://videoblog.lacerba.io/feed/"){maxEntry = 5;}
 		$('#pageTitle').text($(this).text());
 		$('.navbar-toggle').click();
 		$("#loader").fadeIn();
 		$("#rss-feeds").empty();
-		$("#rss-feeds")
-	  .hide()
-	  .rss($(this).attr('href'), {
-		limit: 100,
-		effect: 'slideFastSynced',
-		layoutTemplate: '{entries}',
-		entryTemplate: '<div class="entry"><a href="{url}feed/?withoutcomments=1&fsk=58ce669950cc7" class="entry">{teaserImage}<h3>{title}</h3></a></div>'
-	  }, function() {
-		$("#loader").hide();
-		$("#rss-feeds").show();
-	  });
+		
+		currentURL = $(this).attr('href');
+		$.ajax({
+		  url: currentURL+"json?callback=callback",
+		  method: "GET",
+		  dataType: "jsonp",
+		  data: '',
+		  success: function( data, status, jqxhr ){
+			  console.log( data );
+			$.each(data, function(index, element) {
+			   $('#rss-feeds').append($('<div class="entry"><a href="'+element.permalink+'feed/json" class="entry"><img src="'+element.thumbnail.split(",")[2].split(' 768w')[0]+'"><h3>'+element.title+'</h3></a></div>'
+				));
+			});
+			$("#loader").hide();
+			$("#rss-feeds").show();
+		  },
+		  error: function( jqxhr, status, error ){
+			console.log( "Something went wrong!" );
+		  }
+		});
 	});
 	
 	/*Click sulla entry nella lista*/
@@ -42,17 +60,27 @@ $( document ).ready(function() {
 		$('#pageTitle').text($(this).text());
 		$("#loader").fadeIn();
 		$("#rss-feeds").empty();
-		$("#rss-feeds")
-	  .hide()
-	  .rss($(this).attr('href'), {
-		limit: 100,
-		effect: 'slideFastSynced',
-		layoutTemplate: '{entries}',
-		entryTemplate: '<div class="entry">{body}</div>'
-	  }, function() {
-		$("#loader").hide();
-		$("#rss-feeds").show();
-	  });
+		
+		
+		currentURL = $(this).attr('href');
+		$.ajax({
+		  url: currentURL,
+		  method: "GET",
+		  dataType: "jsonp",
+		  data: '',
+		  success: function( data, status, jqxhr ){
+			  console.log( data );
+			$.each(data, function(index, element) {
+			   $('#rss-feeds').append($('<div class="entry">'+element.content.replace(/(?:https:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(.+)/g, '<iframe width="100%" height="150" src="http://www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe>')+'</div>'
+				));
+			});
+			$("#loader").hide();
+			$("#rss-feeds").show();
+		  },
+		  error: function( jqxhr, status, error ){
+			console.log( "Something went wrong!" );
+		  }
+		});
 	});
 	
 	
