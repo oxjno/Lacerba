60d35e0b6ecd22f79c677e48257c2e71f3d403e6
superdioz <julianus@oxjno.com>
2017-03-29 10:28:01 +0200

    fix fire deviceready

--- js/main.js
+++ js/main.js
@@ -1,19 +1,6 @@
-$( document ).ready(function() {
-	
-	window.isphone = false;
-    if(document.URL.indexOf("http://") === -1 
-        && document.URL.indexOf("https://") === -1) {
-        window.isphone = true;
-    }
-
-    if( window.isphone ) {
-        document.addEventListener("deviceready", onDeviceReady, false);
-    } else {
-        onDeviceReady();
-    }
-
-	
-});
+document.addEventListener("deviceready", function(){
+	onDeviceReady();
+}
 
 
 function onDeviceReady(){
