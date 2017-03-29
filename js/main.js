afb86fc3c1b9199ea904090a2c68a3ac1c215269
superdioz <julianus@oxjno.com>
2017-03-29 10:22:18 +0200

    Full cached feed for 1h

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 $( document ).ready(function() {
 	
+	window.isphone = false;
+    if(document.URL.indexOf("http://") === -1 
+        && document.URL.indexOf("https://") === -1) {
+        window.isphone = true;
+    }
+
+    if( window.isphone ) {
+        document.addEventListener("deviceready", onDeviceReady, false);
+    } else {
+        onDeviceReady();
+    }
+
 	
+});
+
+
+function onDeviceReady(){
 	//https://gist.github.com/anhang/1096149
 	// Changed slightly for testing speed (https://gist.github.com/porkeypop/1096149)
 	var ls2 = {
@@ -18,35 +34,42 @@ $( document ).ready(function() {
 			return (new Date().getTime() < record.timestamp && JSON.parse(record.value));
 		}
 	}
-	
-	/*
-	  var result = ls2.load('a');	
-	  if(result === false) {
-	  }
-	*/
-	
-		
+
 	
 	
 	/*Caricamento iniziale degli ultimi video*/
-	$.ajax({
-	  url: "https://videoblog.lacerba.io/feed/json?callback=callback",
-	  method: "GET",
-	  dataType: "jsonp",
-	  data: '',
-	  success: function( data, status, jqxhr ){
-		ls2.save('https://videoblog.lacerba.io/feed/json?callback=callback', data, 3600000);
-		$.each(data, function(index, element) {
-           $('#rss-feeds').append($('<div class="entry"><a href="'+element.permalink+'feed/json" class="entry"><img src="'+element.thumbnail.split(",")[2].split(' 768w')[0]+'"><h3>'+element.title+'</h3></a></div>'
-            ));
-        });
-		$("#loader").hide();
-		$("#rss-feeds").show();
-	  },
-	  error: function( jqxhr, status, error ){
-		console.log( "Something went wrong!" );
-	  }
-	});
+	currentURL = "https://videoblog.lacerba.io/feed/json?callback=callback";
+	var cached = ls2.load(currentURL);	
+		if(cached === false) {
+			$.ajax({
+			  url: "https://videoblog.lacerba.io/feed/json?callback=callback",
+			  method: "GET",
+			  dataType: "jsonp",
+			  data: '',
+			  success: function( data, status, jqxhr ){
+				ls2.save('https://videoblog.lacerba.io/feed/json?callback=callback', data, 3600000);
+				$.each(data, function(index, element) {
+				   $('#rss-feeds').append($('<div class="entry"><a href="'+element.permalink+'feed/json" class="entry"><img src="'+element.thumbnail.split(",")[2].split(' 768w')[0]+'"><h3>'+element.title+'</h3></a></div>'
+					));
+				});
+				$("#loader").hide();
+				$("#rss-feeds").show();
+			  },
+			  error: function( jqxhr, status, error ){
+				console.log( "Something went wrong!" );
+			  }
+			});
+		}else{
+			console.log('reading from cache');
+			console.log( cached );
+			data = cached;
+			$.each(data, function(index, element) {
+			   $('#rss-feeds').append($('<div class="entry"><a href="'+element.permalink+'feed/json" class="entry"><img src="'+element.thumbnail.split(",")[2].split(' 768w')[0]+'"><h3>'+element.title+'</h3></a></div>'
+				));
+			});
+			$("#loader").hide();
+			$("#rss-feeds").show();
+		}
 	
 	/*Click sul menu*/
 	$(".dropdown-menu a").click(function(e) {
@@ -61,24 +84,42 @@ $( document ).ready(function() {
 		$("#rss-feeds").empty();
 		
 		currentURL = $(this).attr('href');
-		$.ajax({
-		  url: currentURL+"json?callback=callback",
-		  method: "GET",
-		  dataType: "jsonp",
-		  data: '',
-		  success: function( data, status, jqxhr ){
-			  //console.log( data );
+		
+		var cached = ls2.load(currentURL);	
+		if(cached === false) {
+			$.ajax({
+			  url: currentURL+"json?callback=callback",
+			  method: "GET",
+			  dataType: "jsonp",
+			  data: '',
+			  success: function( data, status, jqxhr ){
+				console.log( data );
+				console.log('saving on cache');  
+				ls2.save(currentURL, data, 3600000);
+				$.each(data, function(index, element) {
+				   $('#rss-feeds').append($('<div class="entry"><a href="'+element.permalink+'feed/json" class="entry"><img src="'+element.thumbnail.split(",")[2].split(' 768w')[0]+'"><h3>'+element.title+'</h3></a></div>'
+					));
+				});
+				$("#loader").hide();
+				$("#rss-feeds").show();
+			  },
+			  error: function( jqxhr, status, error ){
+				console.log( "Something went wrong!" );
+			  }
+			});
+		}else{
+			console.log('reading from cache');
+			console.log( cached );
+			data = cached;
 			$.each(data, function(index, element) {
 			   $('#rss-feeds').append($('<div class="entry"><a href="'+element.permalink+'feed/json" class="entry"><img src="'+element.thumbnail.split(",")[2].split(' 768w')[0]+'"><h3>'+element.title+'</h3></a></div>'
 				));
 			});
 			$("#loader").hide();
 			$("#rss-feeds").show();
-		  },
-		  error: function( jqxhr, status, error ){
-			console.log( "Something went wrong!" );
-		  }
-		});
+		}
+		
+		
 	});
 	
 	/*Click sulla entry nella lista*/
@@ -91,25 +132,36 @@ $( document ).ready(function() {
 		
 		
 		currentURL = $(this).attr('href');
-		$.ajax({
-		  url: currentURL,
-		  method: "GET",
-		  dataType: "jsonp",
-		  data: '',
-		  success: function( data, status, jqxhr ){
-			  //console.log( data );
+		var cached = ls2.load(currentURL);	
+		if(cached === false) {
+			$.ajax({
+			  url: currentURL,
+			  method: "GET",
+			  dataType: "jsonp",
+			  data: '',
+			  success: function( data, status, jqxhr ){
+				  //console.log( data );
+				$.each(data, function(index, element) {
+				   $('#rss-feeds').append($('<div class="entry">'+element.content.replace(/(?:https:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(.+)/g, '<iframe width="100%" height="150" src="http://www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe>')+'</div>'
+					));
+				});
+				$("#loader").hide();
+				$("#rss-feeds").show();
+			  },
+			  error: function( jqxhr, status, error ){
+				console.log( "Something went wrong!" );
+			  }
+			});
+		}else{
+			console.log('reading from cache');
+			console.log( cached );
+			data = cached;
 			$.each(data, function(index, element) {
 			   $('#rss-feeds').append($('<div class="entry">'+element.content.replace(/(?:https:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(.+)/g, '<iframe width="100%" height="150" src="http://www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe>')+'</div>'
 				));
 			});
 			$("#loader").hide();
 			$("#rss-feeds").show();
-		  },
-		  error: function( jqxhr, status, error ){
-			console.log( "Something went wrong!" );
-		  }
-		});
+		}	
 	});
-	
-	
-});
+}
